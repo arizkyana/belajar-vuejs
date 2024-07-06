1b552ca2cc3c9ab2ba89d60590f40f059f928568
Muhammad Agung Rizkyana <muhammadagungrizkyana@Muhammads-Mac-mini.local>
2024-07-07 06:39:28 +0700

    chore: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <div class="bg-red-300">
+    <h1>{{ msg }}</h1>
+    <div>{{ count }}</div>
+  </div>
+</template>
+
+<style scoped></style>
