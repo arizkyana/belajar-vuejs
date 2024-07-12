b945f5c9d38107b7d0fa9b827053bf078d9c888b
agungrizkyana <agung.rizkyana@gmail.com>
2024-07-12 14:09:27 +0700

    feat: add toggle theme, improve crud todo

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,13 +1,22 @@
 <script setup>
 import { ref, computed, toRaw } from "vue";
+import collect from "collect.js";
 
 const tasksStorage = localStorage.getItem("tasks")
   ? JSON.parse(localStorage.getItem("tasks"))
   : [];
 
-const filter = ref("all");
+const themeStorage = localStorage.getItem("theme")
+  ? localStorage.getItem("theme")
+  : "dark";
+
+const theme = ref(themeStorage);
 const tasks = ref(tasksStorage);
+const filter = ref("all");
 const inputNewTask = ref("");
+
+// init set theme to body
+document.documentElement.className = theme.value;
 
 const handleCheck = (idx, e) => {
   const isCompleted = e.target.checked;
@@ -54,30 +63,44 @@ const handleSubmit = (e) => {
   currentTasks.push({
     title: inputNewTask.value,
     createdAt: new Date(),
+    isCompleted: false,
   });
 
-  tasks.value = currentTasks;
+  const sortByCreatedAt = collect(currentTasks).sortByDesc("createdAt");
 
-  updateTaskStorage(currentTasks);
+  tasks.value = sortByCreatedAt;
+
+  updateTaskStorage(sortByCreatedAt);
 
   inputNewTask.value = "";
 };
 
+const handleClear = (e) => {
+  e.preventDefault();
+  const currentTasks = [...tasks.value];
+
+  const filtered = currentTasks.filter((item) => !item.isCompleted);
+
+  tasks.value = filtered;
+
+  updateTaskStorage(filtered);
+};
+
 const filterUncompletedTasks = computed(() => {
-  const _tasks = [...tasks.value];
-  return _tasks.filter((item) => !item.isCompleted);
+  const currentTasks = [...tasks.value];
+  return currentTasks.filter((item) => !item.isCompleted);
 });
 
 const filterTasks = computed(() => {
-  const _tasks = [...tasks.value].sort();
+  const currentTasks = [...tasks.value];
 
   if (filter.value === "active")
-    return _tasks.filter((item) => !item.isCompleted);
+    return currentTasks.filter((item) => !item.isCompleted);
   if (filter.value === "completed") {
-    return _tasks.filter((item) => item.isCompleted);
+    return currentTasks.filter((item) => item.isCompleted);
   }
 
-  return _tasks;
+  return collect(currentTasks).sortByDesc("createdAt");
 });
 
 function updateTaskStorage(tasks) {
@@ -85,112 +108,148 @@ function updateTaskStorage(tasks) {
 
   localStorage.setItem("tasks", JSON.stringify(toRaws));
 }
+
+function toggleTheme() {
+  document.documentElement.classList.remove(theme.value);
+
+  const currentTheme = theme.value === "dark" ? "light" : "dark";
+  theme.value = currentTheme;
+  localStorage.setItem("theme", currentTheme);
+
+  document.documentElement.classList.add(currentTheme);
+}
 </script>
 <template>
-  <div class="max-h-screen h-full overflow-y-auto overflow-x-hidden relative">
-    <!-- <div class="bg-blue-600 h-[30vh] fixed top-0 left-0 w-full"></div> -->
-    <div
-      class="max-w-lg mx-auto bg-slate-900 min-h-screen top-0 left-[50%] pb-16"
-    >
-      <section id="header" class="px-6 pb-8 pt-16 bg-blue-500">
-        <h1 class="text-3xl text-white">Mau ngapain hari ini?</h1>
-
-        <form @submit="(event) => handleSubmit(event)" class="my-8">
-          <div class="bg-gray-800 rounded-lg text-gray-200 p-5">
-            <input
-              v-model="inputNewTask"
-              type="text"
-              placeholder="Buat satu aktivitas baru..."
-              class="bg-transparent w-full block outline-none"
-              autofocus
-            />
-          </div>
-        </form>
-      </section>
-      <section id="content" class="px-6 transform -translate-y-8">
-        <div class="w-full bg-slate-800 rounded-lg max-h-max min-h-[4rem]">
-          <label
-            v-for="(task, index) in filterTasks"
-            :key="index"
-            class="px-3 py-5 border-b block cursor-pointer text-slate-200 border-slate-600"
-          >
-            <div class="flex justify-between items-center">
-              <div class="flex justify-start items-center gap-3">
-                <div>
-                  <input
-                    type="checkbox"
-                    class="hidden"
-                    @change="(event) => handleCheck(index, event)"
-                  />
-                  <vue-feather
-                    v-if="task.isCompleted"
-                    type="check-circle"
-                    class="text-emerald-500"
-                  ></vue-feather>
-                  <vue-feather v-else type="circle"></vue-feather>
-                </div>
-                <div class="mb-1">
-                  {{ task.title }}
-                </div>
-              </div>
-
-              <div>
-                <button
-                  type="button"
-                  class="text-red-600 hover:text-red-700"
-                  @click="(event) => handleRemove(index, event)"
-                >
-                  <vue-feather type="trash"></vue-feather>
-                </button>
-              </div>
+  <div class="h-full overflow-y-auto overflow-x-hidden relative">
+    <div class="w-full dark:bg-slate-900 bg-slate-100 min-h-screen pb-16">
+      <div class="max-w-lg mx-auto">
+        <section id="header" class="px-6 pb-8 pt-16">
+          <div class="flex justify-between items-center py-2">
+            <h1 class="text-3xl dark:text-white font-semibold">
+              Mau ngapain hari ini?
+            </h1>
+            <div>
+              <button
+                type="button"
+                class="px-3 py-2 rounded-lg hover:bg-slate-700 hover:text-slate-400 dark:text-slate-300 flex justify-center items-center"
+                @click="toggleTheme"
+              >
+                <vue-feather
+                  :type="theme === 'dark' ? 'moon' : 'sun'"
+                ></vue-feather>
+              </button>
             </div>
-          </label>
-          <div class="px-3 py-5 text-slate-200">
+          </div>
+          <form @submit="(event) => handleSubmit(event)" class="my-8">
             <div
-              class="flex justify-between items-center text-sm text-slate-500"
+              class="dark:bg-slate-800 dark:border-0 border bg-slate-50 rounded-lg dark:text-gray-200 p-5"
             >
-              <div>{{ filterUncompletedTasks.length }} items left</div>
-              <div>
-                <button type="button" class="hover:text-slate-600">
-                  Clear completed
-                </button>
+              <input
+                v-model="inputNewTask"
+                type="text"
+                placeholder="Buat satu aktivitas baru..."
+                class="bg-transparent w-full block outline-none"
+                autofocus
+              />
+            </div>
+          </form>
+        </section>
+        <section id="content" class="px-6 transform -translate-y-8">
+          <div
+            class="w-full dark:bg-slate-800 bg-slate-200 rounded-lg max-h-max min-h-[4rem]"
+          >
+            <label
+              v-for="(task, index) in filterTasks"
+              :key="index"
+              class="px-3 py-5 border-b block cursor-pointer dark:text-slate-200 dark:border-slate-600 border-slate-500"
+            >
+              <div class="flex justify-between items-center">
+                <div class="flex justify-start items-center gap-3">
+                  <div>
+                    <input
+                      type="checkbox"
+                      class="hidden"
+                      @change="(event) => handleCheck(index, event)"
+                    />
+                    <vue-feather
+                      v-if="task.isCompleted"
+                      type="check-circle"
+                      class="text-emerald-500"
+                    ></vue-feather>
+                    <vue-feather v-else type="circle"></vue-feather>
+                  </div>
+                  <div
+                    :class="{ 'line-through': task.isCompleted }"
+                    class="mb-1"
+                  >
+                    {{ task.title }}
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="button"
+                    class="text-red-600 hover:text-red-700"
+                    @click="(event) => handleRemove(index, event)"
+                  >
+                    <vue-feather type="trash"></vue-feather>
+                  </button>
+                </div>
+              </div>
+            </label>
+            <div class="px-3 py-5 text-slate-200">
+              <div
+                class="flex justify-between items-center text-sm text-slate-500"
+              >
+                <div>{{ filterUncompletedTasks.length }} kegiatan tersisa</div>
+                <div>
+                  <button
+                    type="button"
+                    class="hover:text-slate-600"
+                    @click="handleClear"
+                  >
+                    Hapus yang sudah selesai
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-      <section id="filter" class="px-6">
-        <div
-          class="bg-slate-800 px-3 py-5 text-slate-20 font-bold text-slate-500 rounded-lg"
-        >
-          <div class="flex justify-center items-center gap-3">
-            <button
-              :class="{ 'text-blue-600 duration-100': filter === 'all' }"
-              type="button"
-              class="hover:text-blue-600 transition-all"
-              @click="(event) => handleFilter('all', event)"
-            >
-              All
-            </button>
-            <button
-              :class="{ 'text-blue-600 duration-100': filter === 'active' }"
-              type="button"
-              class="hover:text-blue-600 transition-all"
-              @click="(event) => handleFilter('active', event)"
-            >
-              Active
-            </button>
-            <button
-              :class="{ 'text-blue-600 duration-100': filter === 'completed' }"
-              type="button"
-              class="hover:text-blue-600 transition-all"
-              @click="(event) => handleFilter('completed', event)"
-            >
-              Completed
-            </button>
+        </section>
+        <section id="filter" class="px-6">
+          <div
+            class="dark:bg-slate-800 px-3 py-5 text-slate-20 font-bold text-slate-500 rounded-lg"
+          >
+            <div class="flex justify-center items-center gap-3">
+              <button
+                :class="{ 'text-blue-600 duration-100': filter === 'all' }"
+                type="button"
+                class="hover:text-blue-600 transition-all"
+                @click="(event) => handleFilter('all', event)"
+              >
+                Semua
+              </button>
+              <button
+                :class="{ 'text-blue-600 duration-100': filter === 'active' }"
+                type="button"
+                class="hover:text-blue-600 transition-all"
+                @click="(event) => handleFilter('active', event)"
+              >
+                Aktif
+              </button>
+              <button
+                :class="{
+                  'text-blue-600 duration-100': filter === 'completed',
+                }"
+                type="button"
+                class="hover:text-blue-600 transition-all"
+                @click="(event) => handleFilter('completed', event)"
+              >
+                Selesai
+              </button>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </div>
     </div>
   </div>
 </template>
